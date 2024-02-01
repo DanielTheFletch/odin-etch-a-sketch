f741546da93c50f0b178070ba09beb38c2b77eb7
Daniel Fletcher <90067982+DanielTheFletch@users.noreply.github.com>
2024-01-31 22:33:57 -0500

    Create 16x16 grid of square divs

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 // Daniel Fletcher
 // The Odin Project: Foundations
 // Etch-a-Sketch
+
+// Create grid
+const container = document.querySelector('.container');
+for (let i = 0; i < 256; i++)
+{
+    const square = document.createElement('div');
+    square.classList.add('square');
+    square.style.flex = `1 0 ${(1 / 16) * 100}%`;
+    container.appendChild(square);
+}
